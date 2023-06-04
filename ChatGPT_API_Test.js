65766c174395a147653efc51f1bf46bc09a9643a
leo <leoyh.97@gmail.com>
2023-06-04 19:38:28 +0800

    first commit

---
+++ ChatGPT_API_Test.js
@@ -0,0 +1,176 @@
+import { Configuration, OpenAIApi } from "openai";
+import { config } from 'dotenv';
+import axios from 'axios';
+import { performance } from 'perf_hooks';
+import { OpenAI } from "langchain/llms/openai";
+
+// Load environment configuration
+config();
+
+// Check if the OpenAI API Key is present in the environment variables
+if (!process.env.OPENAI_API_KEY) {
+    console.error("Missing OpenAI API key in environment");
+}
+
+// Configure the OpenAI API with the API key from the environment variables
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+// Initialize the OpenAI API with the configuration
+const openai = new OpenAIApi(configuration);
+
+// Initialize the LangChain API
+const model = new OpenAI({
+    openAIApiKey: process.env.OPENAI_API_KEY,
+    temperature: 0,
+});
+
+// Define the test data
+const testData = {
+    model: "gpt-3.5-turbo",
+    messages: [{"role": "system", "content": "Hello, how can I assist you today?"}, {"role": "user", "content": "Tell me a joke"}],
+    temperature: 0,
+    max_tokens: 1000,
+    top_p: 0.95,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    stop: null
+};
+
+// Define the test results for OpenAI package and Axios
+let testResultsOpenAI = {
+    totalRequests: 0,
+    successfulRequests: 0,
+    failedRequests: 0,
+    totalTime: 0,
+};
+
+let testResultsAxios = {
+    totalRequests: 0,
+    successfulRequests: 0,
+    failedRequests: 0,
+    totalTime: 0,
+};
+
+let testResultsLangChain = {
+    totalRequests: 0,
+    successfulRequests: 0,
+    failedRequests: 0,
+    totalTime: 0,
+};
+
+// Function to generate a chat completion using OpenAI's API
+const generateChatCompletionOpenAI = async (content) => {
+    testResultsOpenAI.totalRequests++;
+    let startTime = performance.now();
+
+    try {
+        const response = await openai.createChatCompletion(content);
+
+        // Check the status of the response
+        if (response.status !== 200) {
+            throw new Error(`Error status: ${response.status}`);
+        } else {
+            testResultsOpenAI.successfulRequests++;
+        }
+    } catch (e) {
+        console.log("Error getting GPT completion with OpenAI package: ", e);
+        testResultsOpenAI.failedRequests++;
+    }
+
+    let endTime = performance.now();
+    let duration = endTime - startTime;
+    testResultsOpenAI.totalTime += duration;
+}
+
+// Function to generate a chat completion using axios
+const generateChatCompletionAxios = async (content) => {
+    testResultsAxios.totalRequests++;
+    let startTime = performance.now();
+
+    try {
+        const response = await axios.post('https://api.openai.com/v1/chat/completions', content, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
+            }
+        });
+
+        // Check the status of the response
+        if (response.status !== 200) {
+            throw new Error(`Error status: ${response.status}`);
+        } else {
+            testResultsAxios.successfulRequests++;
+        }
+    } catch (e) {
+        console.log("Error getting GPT completion with Axios: ", e);
+        testResultsAxios.failedRequests++;
+    }
+
+    let endTime = performance.now();
+    let duration = endTime - startTime;
+    testResultsAxios.totalTime += duration;
+}
+
+const generateChatCompletionLangChain = async () => {
+    testResultsLangChain.totalRequests++;
+    let startTime = performance.now();
+
+    try {
+        const response = await model.call(
+            "Tell me a joke"
+        );
+
+        // Check the status of the response
+        if (response === undefined) {
+            throw new Error(`Error status: ${e}`);
+        } else {
+            testResultsLangChain.successfulRequests++;
+        }
+    } catch (e) {
+        console.log("Error getting GPT completion with LangChain: ", e);
+        testResultsLangChain.failedRequests++;
+    }
+
+    let endTime = performance.now();
+    let duration = endTime - startTime;
+    testResultsLangChain.totalTime += duration;
+}
+
+// Run the tests
+(async () => {
+    try {
+        // Call each method 100 times
+        for (let i = 0; i < 100; i++) {
+            await generateChatCompletionOpenAI(testData);
+            await generateChatCompletionAxios(testData);
+            await generateChatCompletionLangChain();
+        }
+
+        // Log the results
+        console.log("Results for OpenAI package:");
+        console.log(`Total requests: ${testResultsOpenAI.totalRequests}`);
+        console.log(`Successful requests: ${testResultsOpenAI.successfulRequests}`);
+        console.log(`Failed requests: ${testResultsOpenAI.failedRequests}`);
+        console.log(`Total time: ${testResultsOpenAI.totalTime}ms`);
+        console.log(`Average time per request: ${testResultsOpenAI.totalTime / testResultsOpenAI.totalRequests}ms`);
+
+        console.log("Results for Axios:");
+        console.log(`Total requests: ${testResultsAxios.totalRequests}`);
+        console.log(`Successful requests: ${testResultsAxios.successfulRequests}`);
+        console.log(`Failed requests: ${testResultsAxios.failedRequests}`);
+        console.log(`Total time: ${testResultsAxios.totalTime}ms`);
+        console.log(`Average time per request: ${testResultsAxios.totalTime / testResultsAxios.totalRequests}ms`);
+
+        console.log("Results for LangChain:");
+        console.log(`Total requests: ${testResultsLangChain.totalRequests}`);
+        console.log(`Successful requests: ${testResultsLangChain.successfulRequests}`);
+        console.log(`Failed requests: ${testResultsLangChain.failedRequests}`);
+        console.log(`Total time: ${testResultsLangChain.totalTime}ms`);
+        console.log(`Average time per request: ${testResultsLangChain.totalTime / testResultsLangChain.totalRequests}ms`);
+    } catch (err) {
+        console.error(`Test failed: ${err}`);
+    }
+})();
+
